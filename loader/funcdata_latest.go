56847703d1a6c81fe3aa53f330ad5ada13c8860f
duanyi.aster <duanyi.aster@bytedance.com>
2023-04-05 23:06:47 +0800

    update Go1.18

--- loader/funcdata_latest.go
+++ loader/funcdata_latest.go
@@ -401,7 +401,7 @@ func makePctab(funcs []Func, cuOffset uint32, nameOffset []int32) (pctab []byte,
             var ab []byte
             var err error
             if pc != nil {
-                ab, err = pc.MarshalBinary()
+                ab, err = pc.MarshalBinary(f.EntryOff+f.TextSize)
                 if err != nil {
                     panic(err)
                 }
